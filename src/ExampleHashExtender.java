da39bf98a33d6bd873223e78a96d56b50a4f6cd1
Erlend Oftedal <erlend@oftedal.no>
2011-12-13 10:42:40 +0100

    Updating demo

--- src/ExampleHashExtender.java
+++ src/ExampleHashExtender.java
@@ -1,48 +1,49 @@
 import java.security.Security;
 
 import org.bouncycastle.crypto.digests.GeneralDigest;
-import org.bouncycastle.crypto.digests.MD5Digest;
 import org.bouncycastle.crypto.digests.SHA1Digest;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Hex;
 
-public class HashExtender {
+public class ExampleHashExtender {
 	public static void main(String[] args) {
 		try {
 			Security.addProvider(new BouncyCastleProvider());
 
-			byte[] input = { 'h', 'e', 'l', 'l', 'o' };
-			byte[] input2 = { 'w', 'o', 'r', 'l', 'd' };
+			byte[] input = "Hello".getBytes();
+			byte[] input2 = "World".getBytes();
 			if (args.length == 2) {
 				input = args[0].getBytes();
 				input2 = args[1].getBytes();
 			}
 
+			//Modify here if you want to use MD5, SHA1 or SHA256
 			GeneralDigest digester = new SHA1Digest();
+
+			//Do a hash of the input
 			digester.update(input, 0, input.length);
 			byte[] digest = new byte[digester.getDigestSize()];
 			digester.doFinal(digest, 0);
+			System.out.println("Original hash: " + new String(Hex.encode(digest)));
 
-			System.out.println(new String(Hex.encode(digest)));
-
+			//Create an extended hash
 			DigestExtender extender = new DigestExtender();
 			byte[] newDigest = extender.extend(digester, digest, input2);
-			System.out.println(new String(Hex.encode(newDigest)));
+			System.out.println("Extended hash: " + new String(Hex.encode(newDigest)));
+
+			//Calculate the padding and build a hash from scratch
 			byte[] pad = extender.getPad(digester,input.length);
-			
-			
 			byte[] padded = join(input, pad);
 			digester.reset();
 			digester.update(padded, 0, padded.length);
-			//extender.printParams(digester);
-			
+			System.out.println("Padding      : " + new String(Hex.encode(pad)));
 			byte[] full = join(padded, input2);
-			System.out.println(new String(Hex.encode(full)));
+			System.out.println("Full input   : " + new String(Hex.encode(full)));
 			digester.reset();
 			digester.update(full, 0, full.length);
 			digester.doFinal(digest, 0);
-			System.out.println(new String(Hex.encode(digest)));
-			
+			System.out.println("Full hash    : " + new String(Hex.encode(digest)));
+			System.out.println("If everything went ok, the full hash should be equal to the extended hash");
 			
 		} catch (Exception ex) {
 			ex.printStackTrace();
